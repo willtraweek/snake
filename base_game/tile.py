b97abd979ba8d02561be9a4640b56394e474d8fe
Will Traweek <will.traweek@gmail.com>
2021-02-22 04:17:58 -0600

    add basic form of a tile

---
+++ base_game/tile.py
@@ -0,0 +1,21 @@
+import pygame
+
+
+class Tile:
+    def __init__(self, pos: (int, int), size: int, color: (int, int, int) = (0, 0, 0)):
+        self.surface = pygame.Surface((size, size))
+        self.pos = pos
+        self.color = color
+        self.rect = self.surface.get_rect(center=pos)
+
+    def draw(self, display_surface):
+        display_surface.blit(self.surface, self.rect)
+
+    @property
+    def color(self):
+        return self.__color
+
+    @color.setter
+    def color(self, color: (int, int, int) = (0, 0, 0)):
+        self.__color = color
+        self.surface.fill(color)
