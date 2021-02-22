5d4b2a34042d4062bebf2cfa79458be504f6ed79
Will Traweek <will.traweek@gmail.com>
2021-02-22 04:39:53 -0600

    PEP-8 compliance

--- base_game/board.py
+++ base_game/board.py
@@ -6,15 +6,15 @@ class Board:
         self.num_tiles = num_tiles
         self.tiles = {}
 
-        offset = size//num_tiles // 2  # THIS ALLOWS US TO CENTER THE TILES
+        offset = size // num_tiles // 2  # THIS ALLOWS US TO CENTER THE TILES
 
         for x in range(num_tiles):
-            pos_x = (size//num_tiles) * x + offset
+            pos_x = (size // num_tiles) * x + offset
 
             for y in range(num_tiles):
-                pos_y = (size//num_tiles) * y + offset
+                pos_y = (size // num_tiles) * y + offset
 
-                self.tiles[(x, y)] = Tile((pos_x, pos_y), size//num_tiles)
+                self.tiles[(x, y)] = Tile((pos_x, pos_y), size // num_tiles)
 
         self.setup_references()
 
@@ -23,14 +23,13 @@ class Board:
             for y in range(self.num_tiles):
                 tile = self.tiles[(x, y)]
                 if x != 0:
-                    tile.north = self.tiles[x-1, y]
+                    tile.north = self.tiles[x - 1, y]
                 if x != self.num_tiles - 1:  # NEEDS THE -1 BECAUSE 0-BASED INDEXING
-                    tile.south = self.tiles[x+1, y]
+                    tile.south = self.tiles[x + 1, y]
                 if y != 0:
-                    tile.west = self.tiles[x, y-1]
+                    tile.west = self.tiles[x, y - 1]
                 if y != self.num_tiles - 1:
-                    tile.east = self.tiles[x, y+1]
-
+                    tile.east = self.tiles[x, y + 1]
 
     def draw(self, display_surface):
         for tile in self.tiles.values():
