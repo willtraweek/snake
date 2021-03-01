dcf89a1519addcb7e33f92ecfe3eb9e2754b0efa
Will Traweek <will.traweek@gmail.com>
2021-03-01 01:37:35 -0600

    add DNA.__init__

--- ai/agent.py
+++ ai/agent.py
@@ -3,9 +3,17 @@ from base_game.tile import Direction
 import Math
 
 class DNA:
-    mutation_rate = .05
-    fitness = 0
+    mutation_rate = 10  # THE PERCENT CHANCE FOR A RANDOM MUTATION TO OCCUR
+    fitness = 1  # INIT THIS VALUE TO 1
+    depth = 5  # HIDDEN NODES LAYER COUNT = DEPTH - 1
+    hidden_length = 15  # HOW MANY NODES IN EACH HIDDEN LAYER
+    input_length = 0
+    output_length = 4
 
+    def __init__(self, mother=None, father=None):
+        if self.input_length == 0:
+            raise RuntimeError("Please set input length")
+        self.genes = []
 
 class Population:
     def __init__(self, population_size):
